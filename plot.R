e895ec363cc52c2e3aed79d4a28576c319a14bde
JCatwood <jcao2416@gmailc.om>
2024-10-28 09:17:57 -0500

    Changed plotting setup

--- plot.R
+++ plot.R
@@ -12,9 +12,18 @@ for (scene in c("Constant", "matern15", "PiecewisePolynomialKernel",
   colnames(df) <- c("m", "MSE", "method")
   df$m <- as.integer(df$m)
   df$MSE <- as.numeric(df$MSE)
+  df$method[df$method == "NNSPGP"] <- "NN-SPGP"
   df$method <- factor(df$method, levels = unique(df$method))
+  df[["RMSE"]] <- sqrt(df$MSE)
   
-  ggplot(data = df, mapping = aes(x = m, y = MSE)) +
-    geom_line(mapping = aes(colour = method, group = method))
-  ggsave(paste0(scene, "_MSE.pdf"), width = 5, height = 5)
+  ggplot(data = df, mapping = aes(x = m, y = RMSE)) +
+    geom_line(mapping = aes(colour = method, group = method), linewidth = 1.4) +
+    theme_minimal() +
+    theme(
+      text = element_text(size = 18),
+      legend.title = element_blank(),
+      legend.position = c(0.8, 0.8),
+      plot.title = element_text(hjust = 0.5)
+    )
+  ggsave(paste0(scene, "_RMSE.pdf"), width = 5, height = 5)
 }
