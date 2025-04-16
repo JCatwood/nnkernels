49807e10aedeab361272ec3f96eb05230f9f4fc9
JCatwood <jcao2416@gmailc.om>
2025-04-15 22:58:44 -0500

    Separate loss_func and seq generation. Ran cond sd training. Going to run cond mean training

--- nn_vecchia/lstm.py
+++ nn_vecchia/lstm.py
@@ -3,15 +3,12 @@ import gpytorch
 import random
 import os
 import sys
+import time
+
+from loss_func import prepare_sequence_cond_sd
+from loss_func import loss_len10_cond_sd, loss_len1to30_cond_sd
 
 torch.manual_seed(1)
-
-def prepare_sequence(length, kernel, nbatch = 1):
-    locs = torch.rand([nbatch, length, 2])
-    covmat = kernel(locs)
-    L = torch.linalg.cholesky(covmat)
-    return locs, L.to_dense()[:, -1, -1]
-
 
 class LSTMKernel(torch.nn.Module):
     def __init__(self, nFeature, nHidden):
@@ -39,35 +36,54 @@ if len(sys.argv) > 1:
     n_batch = int(sys.argv[3])
 else:
     n_hidden = 256
-    n_epoch = 10000
+    n_epoch = 40000
     n_batch = 1000
 model = LSTMKernel(2, n_hidden)
 loss_function = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 kernel = gpytorch.kernels.MaternKernel(1.5)
-
-model.train()
-for epoch in range(n_epoch):  
-    n_locs = 10
-    # n_locs = random.randint(1, 30)
-    locs, cond_sd = prepare_sequence(n_locs, kernel, n_batch)
-    optimizer.zero_grad()
-    cond_sd_pred, _, _ = model(locs, None, None)
-    loss = loss_function(cond_sd_pred, cond_sd)
-    loss.backward()
-    optimizer.step()
-    if epoch % 1000 == 0:
-        print(f"Loss after {epoch} iterations is {loss.detach().item()}", flush=True)
+fixed_len = False
 
 os.makedirs("trained_models", exist_ok=True)
-torch.save(model.state_dict(), f"trained_models/cond_sd_len10_LSTM_{n_hidden}_{n_epoch}_{n_batch}.pt")
-# torch.save(model.state_dict(), f"trained_models/cond_sd_len1-30_LSTM_{n_hidden}_{n_epoch}_{n_batch}.pt")
+model.train()
+timer = time.perf_counter()
+if fixed_len:
+    for epoch in range(n_epoch): 
+        optimizer.zero_grad()
+        loss = loss_len10_cond_sd(model, loss_function, kernel, n_batch)
+        loss.backward()
+        optimizer.step()
+        if epoch % 1000 == 0:
+            print(f"Loss after {epoch} iterations is {loss.detach().item()}", flush=True)
+            timer_prev = timer
+            timer = time.perf_counter()
+            print(f"Elapsed time: {timer - timer_prev } seconds")
+    torch.save(model.state_dict(), f"trained_models/cond_sd_len10_LSTM_{n_hidden}_{n_epoch}_{n_batch}.pt")
+else:
+    for epoch in range(n_epoch): 
+        optimizer.zero_grad()
+        loss = loss_len1to30_cond_sd(model, loss_function, kernel, n_batch)
+        loss.backward()
+        optimizer.step()
+        if epoch % 1000 == 0:
+            print(f"Loss after {epoch} iterations is {loss.detach().item()}", flush=True)
+            timer_prev = timer
+            timer = time.perf_counter()
+            print(f"Elapsed time: {timer - timer_prev } seconds")
+    torch.save(model.state_dict(), f"trained_models/cond_sd_len1-30_LSTM_{n_hidden}_{n_epoch}_{n_batch}.pt")
 
 model.eval()
 with torch.no_grad():
-    # for n_locs in range(1, 31):
-    for n_locs in range(10, 11):
-        locs, cond_sd = prepare_sequence(n_locs, kernel, n_batch)
+    if fixed_len:
+        for n_locs in range(10, 11):
+            locs, cond_sd = prepare_sequence_cond_sd(n_locs, kernel, n_batch)
+            cond_sd_pred, _, _ = model(locs, None, None)
+            loss = loss_function(cond_sd_pred, cond_sd)
+            print(f"At length {n_locs}, var is {cond_sd.var().item()}, MSE is {loss.item()}")
+    else:
+        length = torch.randint(1, 30, (n_batch,))
+        locs, cond_sd = prepare_sequence_cond_sd(length, kernel, n_batch)
         cond_sd_pred, _, _ = model(locs, None, None)
         loss = loss_function(cond_sd_pred, cond_sd)
-        print(f"At length {n_locs}, var is {cond_sd.var().item()}, MSE is {loss.item()}")
+        print(f"with n_loc in [{min(length)}, {max(length)}], "
+              f"var is {cond_sd.var().item()}, MSE is {loss.item()}")
