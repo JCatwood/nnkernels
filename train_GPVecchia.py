1838c8f86eacdc7d583657cace9e7149f431e7ef
JCatwood <jcao2416@gmailc.om>
2026-01-20 10:52:59 -0600

    Output results in json format. Code formatted with Black

--- train_GPVecchia.py
+++ train_GPVecchia.py
@@ -1,6 +1,7 @@
 import torch
 import sys
 import time
+import json
 
 from models import GPVecchia, MyMaternKernel, MyNSKernel_Scale, MyNSKernel_Lengthscale
 from dataloader import Vecc_Dataloader_GP_sim, Vecc_Dataloader_Dataset
@@ -8,24 +9,30 @@ from loss import NllLoss
 
 torch.manual_seed(1)
 # %% tuning parameters
-d = 2 # locs are sampled from R^d
+d = 2  # locs are sampled from R^d
 m = 30
 if len(sys.argv) > 1:
-    if sys.argv[1].lower().strip() in ('yes', 'true', 't', 'y', '1', 'on'):
+    if sys.argv[1].lower().strip() in ("yes", "true", "t", "y", "1", "on"):
         fixed_len = True
-    elif sys.argv[1].lower().strip() in ('no', 'false', 'f', 'n', '0', 'off'):
+    elif sys.argv[1].lower().strip() in ("no", "false", "f", "n", "0", "off"):
         fixed_len = False
     else:
         raise ValueError(f"Invalid boolean value: '{sys.argv[1]}'")
     kernel_train_name = sys.argv[2]
-    assert kernel_train_name in ("MyMaternKernel", "MyNSKernel_Scale", "MyNSKernel_Lengthscale"), \
-            "Invalid kernel_train_name (second) arguments"
+    assert kernel_train_name in (
+        "MyMaternKernel",
+        "MyNSKernel_Scale",
+        "MyNSKernel_Lengthscale",
+    ), "Invalid kernel_train_name (second) arguments"
     train_type = sys.argv[3]
-    assert train_type in ('data', 'simulation'), "Invalid train_type (third) argument"
-    if train_type == 'simulation':
+    assert train_type in ("data", "simulation"), "Invalid train_type (third) argument"
+    if train_type == "simulation":
         kernel_gen_name = sys.argv[4]
-        assert kernel_gen_name in ("MyMaternKernel", "MyNSKernel_Scale", "MyNSKernel_Lengthscale"), \
-            "Invalid kernel_gen_name (fourth) arguments"
+        assert kernel_gen_name in (
+            "MyMaternKernel",
+            "MyNSKernel_Scale",
+            "MyNSKernel_Lengthscale",
+        ), "Invalid kernel_gen_name (fourth) arguments"
     else:
         data_name = sys.argv[4]
         if len(sys.argv) > 5:
@@ -34,21 +41,21 @@ if len(sys.argv) > 1:
             data_seed = None
 else:
     fixed_len = True
-    train_type = "data" # ["simulation", "data"]
+    train_type = "data"  # ["simulation", "data"]
     data_name = "GP_NS_scale_2000_1000"
     data_seed = 0
-    kernel_gen_name = "MyNSKernel_Scale" # ["MyMaternKernel", "MyNSKernel_Scale", "MyNSKernel_Lengthscale"]
-    kernel_train_name = "MyMaternKernel" # ["MyMaternKernel", "MyNSKernel_Scale", "MyNSKernel_Lengthscale"]
+    kernel_gen_name = "MyNSKernel_Scale"  # ["MyMaternKernel", "MyNSKernel_Scale", "MyNSKernel_Lengthscale"]
+    kernel_train_name = "MyMaternKernel"  # ["MyMaternKernel", "MyNSKernel_Scale", "MyNSKernel_Lengthscale"]
 
 # %% model parameters
 if torch.cuda.is_available():
-    device = torch.device('cuda')
+    device = torch.device("cuda")
     print(f"GPU is available. Using device: {torch.cuda.get_device_name(0)}")
     n_batch = 2048
     n_epoch = 30000
 else:
     print("GPU is not available. Using CPU.")
-    device = torch.device('cpu')
+    device = torch.device("cpu")
     n_batch = 1024
     n_epoch = 4000
 # %% dataloader
@@ -60,12 +67,14 @@ if train_type == "simulation":
         kernel_gen = MyNSKernel_Scale(-0.5, -1.2, -1.44, 0.3, 1.5, 0.01)
     elif kernel_gen_name == "MyNSKernel_Lengthscale":
         kernel_gen = MyNSKernel_Lengthscale(-0.5, -1.2, -1.44, 2.0, 0.01)
-    dataloader = Vecc_Dataloader_GP_sim(kernel_gen, d, fixed_len, m + 1, target='y')
+    dataloader = Vecc_Dataloader_GP_sim(kernel_gen, d, fixed_len, m + 1, target="y")
 elif train_type == "data":
     if data_seed is None:
         dataloader = Vecc_Dataloader_Dataset(data_name, fixed_len, length_max=m + 1)
     else:
-        dataloader = Vecc_Dataloader_Dataset(data_name, fixed_len, length_max=m + 1, seed=data_seed)
+        dataloader = Vecc_Dataloader_Dataset(
+            data_name, fixed_len, length_max=m + 1, seed=data_seed
+        )
 else:
     raise Exception("Unexpected train_type")
 # %% initialize model
@@ -80,23 +89,31 @@ elif kernel_train_name == "MyNSKernel_Lengthscale":
     KernelClass = MyNSKernel_Lengthscale
 model = GPVecchia(KernelClass, *kernel_parms_init)
 model.to(device)
+
 # %% scheduler
 def lr_lambda(epoch):
     base_lr = 0.001
     factor = 0.0001
-    return base_lr/(1+factor*epoch)
+    return base_lr / (1 + factor * epoch)
     # return 0.001
-# %% loss func  
+
+
+# %% loss func
 loss_function = NllLoss()
 # %% model training
 optimizer = torch.optim.Adam(model.parameters(), lr=1)
 scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
 model.train()
 timer = time.perf_counter()
-for epoch in range(n_epoch): 
+for epoch in range(n_epoch):
     with torch.no_grad():
         X_batch, y_batch, y_true, length = dataloader.get_minibatch(size=n_batch)
-        X_batch, y_batch, y_true, length = X_batch.to(device), y_batch.to(device), y_true.to(device), length.to(device)
+        X_batch, y_batch, y_true, length = (
+            X_batch.to(device),
+            y_batch.to(device),
+            y_true.to(device),
+            length.to(device),
+        )
     # predict the target
     optimizer.zero_grad()
     y_pred, y_stderr = model(X_batch, y_batch, length=length)
@@ -117,13 +134,40 @@ crt_lr = optimizer.param_groups[0]["lr"]
 print(f"Current LR: {crt_lr}", flush=True)
 print(f"Loss after {epoch} iterations is {loss.detach().item()}", flush=True)
 # %% evaluate
-model.to('cpu')
+model.to("cpu")
 model.eval()
 loss_MSE = torch.nn.MSELoss()
-# compare with GPVecchia initiated with the true kernel
 with torch.no_grad():
-    X_batch, y_batch, y_true, length = dataloader.get_test_batch(size=n_batch)
+    if train_type == "simulation":
+        X_batch, y_batch, y_true, length = dataloader.get_test_batch(size=n_batch)
+    else:
+        X_batch, y_batch, y_true, length = dataloader.get_test_batch(
+            size=dataloader.n_test
+        )
     y_pred, y_stderr = model(X_batch, y_batch, length=length)
     loss = loss_function(y_pred, y_true, y_stderr)
-    print(f"NLL loss of the GP model is {loss.detach().item()}", flush=True)
-    print(f"MSE loss of the GP model is {loss_MSE(y_pred, y_true).item()}", flush=True)
+    print(">>>")
+    if train_type == "simulation":
+        output_dict = {
+            "data_type": train_type,
+            "kernel_sim": kernel_gen_name,
+            "model": "GPVecchia",
+            "m": m,
+            "same_length": fixed_len,
+            "NLL": loss.detach().item(),
+            "MSE": loss_MSE(y_pred, y_true).item(),
+        }
+    else:
+        output_dict = {
+            "data_type": train_type,
+            "data_name": data_name,
+            "seed": data_seed,
+            "model": "GPVecchia",
+            "m": m,
+            "same_length": fixed_len,
+            "NLL": loss.detach().item(),
+            "MSE": loss_MSE(y_pred, y_true).item(),
+        }
+    output_str = json.dumps(output_dict)
+    print(output_str)
+    print("<<<")
--- train_NN.py
+++ train_NN.py
@@ -1,35 +1,45 @@
 import torch
 import time
 import sys
+import json
 
-from models import NNDT_Sum_NNTG, GPVecchia, MyMaternKernel, MyNSKernel_Scale, MyNSKernel_Lengthscale
+from models import (
+    NNDT_Sum_NNTG,
+    GPVecchia,
+    MyMaternKernel,
+    MyNSKernel_Scale,
+    MyNSKernel_Lengthscale,
+)
 from loss import NllLoss
 from dataloader import Vecc_Dataloader_GP_sim, Vecc_Dataloader_Dataset
 from input_transform import input_transformed_dim, input_transform
 
 torch.manual_seed(1)
 # %% tuning parameters
-d = 2 # locs are sampled from R^d
+d = 2  # locs are sampled from R^d
 m = 30
 dropout_ratio = 0.5
-input_trans_type = 'dist_direction_lastloc'
-input_trans_mean = input_trans_type + '_and_y'
+input_trans_type = "dist_direction_lastloc"
+input_trans_mean = input_trans_type + "_and_y"
 input_trans_sd = input_trans_type
 nfeature_mean = input_transformed_dim(d, input_trans_mean)
 nfeature_sd = input_transformed_dim(d, input_trans_sd)
 if len(sys.argv) > 1:
-    if sys.argv[1].lower().strip() in ('yes', 'true', 't', 'y', '1', 'on'):
+    if sys.argv[1].lower().strip() in ("yes", "true", "t", "y", "1", "on"):
         fixed_len = True
-    elif sys.argv[1].lower().strip() in ('no', 'false', 'f', 'n', '0', 'off'):
+    elif sys.argv[1].lower().strip() in ("no", "false", "f", "n", "0", "off"):
         fixed_len = False
     else:
         raise ValueError(f"Invalid boolean value: '{sys.argv[1]}'")
     train_type = sys.argv[2]
-    assert train_type in ('data', 'simulation'), "Invalid train_type (second) argument"
-    if train_type == 'simulation':
+    assert train_type in ("data", "simulation"), "Invalid train_type (second) argument"
+    if train_type == "simulation":
         kernel_gen_name = sys.argv[3]
-        assert kernel_gen_name in ("MyMaternKernel", "MyNSKernel_Scale", "MyNSKernel_Lengthscale"), \
-            "Invalid kernel_gen_name (third) arguments"
+        assert kernel_gen_name in (
+            "MyMaternKernel",
+            "MyNSKernel_Scale",
+            "MyNSKernel_Lengthscale",
+        ), "Invalid kernel_gen_name (third) arguments"
     else:
         data_name = sys.argv[3]
         if len(sys.argv) > 4:
@@ -38,13 +48,13 @@ if len(sys.argv) > 1:
             data_seed = None
 else:
     fixed_len = True
-    train_type = "data" # ["simulation", "data"]
-    kernel_gen_name = "MyNSKernel_Scale" # ["MyMaternKernel", "MyNSKernel_Scale", "MyNSKernel_Lengthscale"]
+    train_type = "data"  # ["simulation", "data"]
+    kernel_gen_name = "MyNSKernel_Scale"  # ["MyMaternKernel", "MyNSKernel_Scale", "MyNSKernel_Lengthscale"]
     data_name = "GP_NS_scale_2000_1000"
     data_seed = 0
 # %% model parameters
 if torch.cuda.is_available():
-    device = torch.device('cuda')
+    device = torch.device("cuda")
     print(f"GPU is available. Using device: {torch.cuda.get_device_name(0)}")
     size_DT_mean = [nfeature_mean, 128, 128, 128, 8]
     size_TG_mean = [8, 128, 128, 128, 1]
@@ -54,10 +64,10 @@ if torch.cuda.is_available():
     n_epoch = 30000
 else:
     print("GPU is not available. Using CPU.")
-    device = torch.device('cpu')
-    size_DT_mean = [nfeature_mean, 96, 96, 4] 
+    device = torch.device("cpu")
+    size_DT_mean = [nfeature_mean, 96, 96, 4]
     size_TG_mean = [4, 96, 96, 1]
-    size_DT_sd = [nfeature_sd, 64, 64, 3] 
+    size_DT_sd = [nfeature_sd, 64, 64, 3]
     size_TG_sd = [3, 32, 32, 1]
     n_batch = 1024
     n_epoch = 4000
@@ -70,12 +80,14 @@ if train_type == "simulation":
         kernel_gen = MyNSKernel_Scale(-0.5, -1.2, -1.44, 0.3, 1.5, 0.01)
     elif kernel_gen_name == "MyNSKernel_Lengthscale":
         kernel_gen = MyNSKernel_Lengthscale(-0.5, -1.2, -1.44, 2.0, 0.01)
-    dataloader = Vecc_Dataloader_GP_sim(kernel_gen, d, fixed_len, m + 1, target='y')
+    dataloader = Vecc_Dataloader_GP_sim(kernel_gen, d, fixed_len, m + 1, target="y")
 elif train_type == "data":
     if data_seed is None:
         dataloader = Vecc_Dataloader_Dataset(data_name, fixed_len, length_max=m + 1)
     else:
-        dataloader = Vecc_Dataloader_Dataset(data_name, fixed_len, length_max=m + 1, seed=data_seed)
+        dataloader = Vecc_Dataloader_Dataset(
+            data_name, fixed_len, length_max=m + 1, seed=data_seed
+        )
 else:
     raise Exception("Unexpected train_type")
 # %% initialize model
@@ -83,28 +95,37 @@ model_mean = NNDT_Sum_NNTG(size_DT_mean, size_TG_mean, dropout=dropout_ratio)
 model_sd = NNDT_Sum_NNTG(size_DT_sd, size_TG_sd, dropout=dropout_ratio)
 model_mean.to(device)
 model_sd.to(device)
+
+
 # %% scheduler
 def lr_lambda(epoch):
     base_lr = 0.001
     factor = 0.0001
-    return base_lr/(1+factor*epoch)
+    return base_lr / (1 + factor * epoch)
     # return 0.001
-# %% loss func  
+
+
+# %% loss func
 loss_function = NllLoss()
 # %% model training
-optimizer = torch.optim.Adam([{'params': model_mean.parameters()}, 
-                              {'params': model_sd.parameters()}], lr=1)
+optimizer = torch.optim.Adam(
+    [{"params": model_mean.parameters()}, {"params": model_sd.parameters()}], lr=1
+)
 scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
 model_mean.train()
 model_sd.train()
 timer = time.perf_counter()
-for epoch in range(n_epoch): 
+for epoch in range(n_epoch):
     with torch.no_grad():
         X_batch, y_batch, y_true, length = dataloader.get_minibatch(size=n_batch)
         input_mean = input_transform(X_batch, y_batch, length, type=input_trans_mean)
         input_sd = input_transform(X_batch, None, length, type=input_trans_sd)
-        input_mean, input_sd, y_true, length = input_mean.to(device), \
-            input_sd.to(device), y_true.to(device), length.to(device)
+        input_mean, input_sd, y_true, length = (
+            input_mean.to(device),
+            input_sd.to(device),
+            y_true.to(device),
+            length.to(device),
+        )
     # predict mean and stderr
     optimizer.zero_grad()
     y_pred = model_mean(input_mean, length=length)
@@ -126,25 +147,58 @@ crt_lr = optimizer.param_groups[0]["lr"]
 print(f"Current LR: {crt_lr}", flush=True)
 print(f"Loss after {epoch} iterations is {loss.detach().item()}", flush=True)
 # %% evaluate
-model_mean.to('cpu')
-model_sd.to('cpu')
+model_mean.to("cpu")
+model_sd.to("cpu")
 model_mean.eval()
 model_sd.eval()
 loss_MSE = torch.nn.MSELoss()
-# compare with GPVecchia initiated with the true kernel
 with torch.no_grad():
-    X_batch, y_batch, y_true, length = dataloader.get_minibatch(size=n_batch)
+    if train_type == "simulation":
+        X_batch, y_batch, y_true, length = dataloader.get_test_batch(size=n_batch)
+    else:
+        X_batch, y_batch, y_true, length = dataloader.get_test_batch(
+            size=dataloader.n_test
+        )
     input_mean = input_transform(X_batch, y_batch, length, type=input_trans_mean)
     input_sd = input_transform(X_batch, None, length, type=input_trans_sd)
-    
+
     y_pred = model_mean(input_mean, length=length)
     y_stderr = torch.exp(model_sd(input_sd, length=length))
     loss = loss_function(y_pred, y_true, y_stderr)
-    print(f"NLL loss of the proposed model is {loss.detach().item()}", flush=True)
-    print(f"MSE loss of the proposed model is {loss_MSE(y_pred, y_true).item()}", flush=True)
-    
-    mdl_GP = GPVecchia(MyMaternKernel, *[1.0, 0.3, 1.5, 0.01])
-    y_pred_test_GP, y_stderr_test_GP = mdl_GP(X_batch, y_batch, length=length)
-    loss_test_GP = loss_function(y_pred_test_GP, y_true, y_stderr_test_GP)
-    print(f"NLL loss of GP is {loss_test_GP.detach().item()}", flush=True)
-    print(f"MSE loss of GP is {loss_MSE(y_pred_test_GP, y_true).item()}", flush=True)
+    print(">>>")
+    if train_type == "simulation":
+        output_dict = {
+            "data_type": train_type,
+            "kernel_sim": kernel_gen_name,
+            "model": "NN",
+            "m": m,
+            "dropout": dropout_ratio,
+            "size_DT_mean": size_DT_mean,
+            "size_TG_mean": size_TG_mean,
+            "size_DT_sd": size_DT_sd,
+            "size_TG_sd": size_TG_sd,
+            "transformation": input_trans_type,
+            "same_length": fixed_len,
+            "NLL": loss.detach().item(),
+            "MSE": loss_MSE(y_pred, y_true).item(),
+        }
+    else:
+        output_dict = {
+            "data_type": train_type,
+            "data_name": data_name,
+            "seed": data_seed,
+            "model": "NN",
+            "m": m,
+            "dropout": dropout_ratio,
+            "size_DT_mean": size_DT_mean,
+            "size_TG_mean": size_TG_mean,
+            "size_DT_sd": size_DT_sd,
+            "size_TG_sd": size_TG_sd,
+            "transformation": input_trans_type,
+            "same_length": fixed_len,
+            "NLL": loss.detach().item(),
+            "MSE": loss_MSE(y_pred, y_true).item(),
+        }
+    output_str = json.dumps(output_dict)
+    print(output_str)
+    print("<<<")
